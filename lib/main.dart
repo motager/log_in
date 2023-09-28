1828076de690a97de9e0157944837f400052e87a
motager <maltager658@gmail.com>
2023-09-28 20:29:30 +0300

    new files
    Signed-off-by: motager <maltager658@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -47,81 +47,84 @@ class English extends StatelessWidget {
        
        
        
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                   width: 300.0,
-            margin: const EdgeInsets.fromLTRB(30.0, 18.0, 30.0, 18.0),
-            // color: Colors.purple[400],
-            decoration: BoxDecoration(
-               color: Colors.purple[100],
-              borderRadius: BorderRadius.circular(50.0),
-            ),
-                child: const TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.person),
-                      
-                      border: InputBorder.none,
-                      hintText: "Enter your email",
-                      labelText: "Your email :",
-                      labelStyle: TextStyle(
-                        fontSize: 21.0,
-                      )),
-                ),
+        body: Center(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center ,
+              children: [
+                Container(
+                     width: 300.0,
+              margin: const EdgeInsets.fromLTRB(30.0, 18.0, 30.0, 18.0),
+              // color: Colors.purple[400],
+              decoration: BoxDecoration(
+                 color: Colors.purple[100],
+                borderRadius: BorderRadius.circular(50.0),
               ),
-              Container(
-              
-                margin: const EdgeInsets.fromLTRB(30.0, 18.0, 30.0, 18.0),
-               
-                decoration: BoxDecoration(
-                   color: Colors.purple[100],
-                  borderRadius: BorderRadius.circular(30.0)
-                ),
-                child: TextField(
-                  textInputAction: TextInputAction.next,
-                  obscureText: true,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.visibility),
-                    border: InputBorder.none,
-                    hintText: "      Enter your password",
-                    labelText: "  Your Password",
-                    labelStyle: TextStyle(
-                      fontSize: 21.0,
-                    ),
-                   prefixIcon: Icon(Icons.lock),  
+                  child: const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        
+                        border: InputBorder.none,
+                        hintText: "Enter your email",
+                        labelText: "Your email :",
+                        labelStyle: TextStyle(
+                          fontSize: 21.0,
+                        )),
                   ),
                 ),
+                Container(
+                              margin: const EdgeInsets.fromLTRB(30.0, 18.0, 30.0, 18.0),              
+          width: 300.0,
+                 
+                    decoration: BoxDecoration(
+                 color: Colors.purple[100],
+                borderRadius: BorderRadius.circular(50.0),
               ),
-                    Container(
-            width: 300.0,
-            margin: const EdgeInsets.fromLTRB(30.0, 18.0, 30.0, 18.0),
-            // color: Colors.purple[400],
-            decoration: BoxDecoration(
-              color: Colors.purple,
-              borderRadius: BorderRadius.circular(90),
+                  child: TextField(
+                    textInputAction: TextInputAction.next,
+                    obscureText: true,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.visibility),
+                      border: InputBorder.none,
+                      hintText: "      Enter your password",
+                      labelText: "  Your Password",
+                      labelStyle: TextStyle(
+                        fontSize: 21.0,
+                      ),
+                     prefixIcon: Icon(Icons.lock),  
+                    ),
+                  ),
+                ),
+                      Container(
+              width: 300.0,
+              margin: const EdgeInsets.fromLTRB(30.0, 18.0, 30.0, 18.0),
+              // color: Colors.purple[400],
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.circular(90),
+              ),
+              child: TextButton(
+                style: ButtonStyle(
+                  
+                ),
+                onPressed: () {
+                BoxDecoration(
+                border: Border.all(style: BorderStyle.solid),
+                              );
+                },
+                child: Text(
+                  "log in",
+                  style: TextStyle(color: Colors.blue, fontSize: 30.0),
+                ),
+              ),
             ),
-            child: TextButton(
-              style: ButtonStyle(
-                
-              ),
-              onPressed: () {
-              BoxDecoration(
-              border: Border.all(style: BorderStyle.solid),
-                            );
-              },
-              child: Text(
-                "log in",
-                style: TextStyle(color: Colors.blue, fontSize: 30.0),
-              ),
+              ],
             ),
+              
           ),
-            ],
-          ),
-      
         ),
       ),
     );
